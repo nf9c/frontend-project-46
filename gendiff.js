8661586fc4b3d876edf9f669e315612ee5c31cda
nf <coderly@yandex.ru>
2023-05-02 12:30:58 +0300

    Stage 1.0.1 Help configured

--- gendiff.js
+++ gendiff.js
@@ -1,9 +1,6 @@
 #!/usr/bin/env node
 
-const { Command } = require('commander');
-const program = new Command();
-
-// import { program } from 'commander';
+import { program } from 'commander';
 
 program
   .version('0.0.1')
