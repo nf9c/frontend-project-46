3c6b247eb28d1277028799b913ae16c492f281da
nf <coderly@yandex.ru>
2023-05-02 13:51:20 +0300

    Stage 2.0.0 Arguments and format added to Help

--- gendiff.js
+++ gendiff.js
@@ -5,4 +5,6 @@ import { program } from 'commander';
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
   .parse();
